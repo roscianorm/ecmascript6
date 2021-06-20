e9a9614466eae117bbfbe950adcd855f722cb2ae
Miguel Rosciano <roscianorm@gmail.com>
2021-06-19 21:55:17 -0600

    default params & template literals

---
+++ default-params.js
@@ -0,0 +1,15 @@
+// Default Params
+function newFunction(name, age, country) {
+  var name = name || 'miguel'
+  var age = age || 22
+  var country = country || 'MX'
+  console.log(name, age, country)
+}
+
+// es6
+function newFunctionES6(name = 'miguel', age = 22, country = 'MX') {
+  console.log(name, age, country)
+}
+
+newFunctionES6()
+newFunctionES6('oscar', 32, 'CO')
---
+++ template-literals.js
@@ -0,0 +1,8 @@
+let hello = 'Hello'
+let world = 'World'
+let epicPhrase = hello + ' ' + world
+console.log(epicPhrase)
+
+// es6
+let epicPhraseES6 = `${hello} ${world}`
+console.log('ES6:', epicPhraseES6)
